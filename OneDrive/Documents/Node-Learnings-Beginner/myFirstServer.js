3a03af7615a43f2400bb7154219364d73623a290
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-02-01 18:18:55 +0530

    doc: Understood the Global Object in Node-JS

---
+++ OneDrive/Documents/Node-Learnings-Beginner/myFirstServer.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/getData") {
+    res.end("Data Chahi Re 🤨");
+    return;
+  } else {
+    res.end("Sun raha hu be: :|");
+  }
+});
+server.listen(3000);
